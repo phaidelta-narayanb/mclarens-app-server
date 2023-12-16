f5cc4cbdb75816d8cfefd2bfe1449256e6bde914
Narayan Bandodker <narayan.b@phaidelta.com>
2023-12-16 17:09:44 +0530

    Updated status code for make report

--- reportserver/resources/report/router.py
+++ reportserver/resources/report/router.py
@@ -1,5 +1,6 @@
 
 from fastapi import FastAPI, APIRouter
+from starlette.status import HTTP_201_CREATED
 
 from . import views
 
@@ -8,7 +9,7 @@ def init_app(app: FastAPI):
     report_routes = APIRouter(prefix="/report")
     reports_routes = APIRouter(prefix="/reports")
 
-    report_routes.add_api_route("", views.make_report, methods={"POST"})
+    report_routes.add_api_route("", views.make_report, methods={"POST"}, status_code=HTTP_201_CREATED)
 
     reports_routes.add_api_route("/", views.get_report_list, methods={"GET"})
     reports_routes.add_api_route("/{report_id}", views.get_report, methods={"GET"})
