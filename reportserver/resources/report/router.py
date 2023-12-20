2a1d6da1e1ed6aac6bd6dc2cdddb005b99c479b8
Narayan Bandodker <narayan.b@phaidelta.com>
2023-12-20 09:26:05 +0530

    Corrected route for report fetch

--- reportserver/resources/report/router.py
+++ reportserver/resources/report/router.py
@@ -10,10 +10,10 @@ def init_app(app: FastAPI):
     reports_routes = APIRouter(prefix="/reports", tags=["report-generator"])
 
     report_routes.add_api_route("", views.make_report, methods={"POST"}, status_code=HTTP_201_CREATED, tags=["task"])
+    report_routes.add_api_route("/{report_id}", views.get_report, methods={"GET"})
+    report_routes.add_api_route("/{report_id}/pdf", views.get_report_pdf, methods={"GET"})
 
     reports_routes.add_api_route("", views.get_report_list, methods={"GET"})
-    reports_routes.add_api_route("/{report_id}", views.get_report, methods={"GET"})
-    reports_routes.add_api_route("/{report_id}/pdf", views.get_report_pdf, methods={"GET"})
 
     app.include_router(report_routes)
     app.include_router(reports_routes)
