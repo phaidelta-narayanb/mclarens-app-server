117279b0557ab5b4c3efc47a327322a22eaf1f80
Narayan Bandodker <narayan.b@phaidelta.com>
2023-12-20 15:44:40 +0530

    Updated task model

--- reportserver/resources/task/models.py
+++ reportserver/resources/task/models.py
@@ -1,4 +1,5 @@
 from datetime import datetime, timedelta
+from typing import Optional
 from uuid import UUID
 from pydantic import BaseModel
 
@@ -6,9 +7,9 @@ from pydantic import BaseModel
 class Task(BaseModel):
     id: str
     status: str
-    progress: float
-    queue_position: int
-    estimated_queue_time: timedelta
+    progress: Optional[float] = None
+    queue_position: Optional[int] = None
+    estimated_queue_time: Optional[timedelta] = None
     created_by_user: UUID
     created_ts: datetime
     updated_ts: datetime
